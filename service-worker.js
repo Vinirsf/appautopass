470fcc1745925d6831219e5cdb1ef0ceab4ea817
Vinirsf <viniryansf@gmail.com>
2025-04-24 17:51:25 -0300

    📦 Adiciona suporte PWA com manifest e service worker

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,39 @@
+const CACHE_NAME = 'lava-rapido-cache-v1';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+
+// Instala o SW e faz cache dos arquivos
 self.addEventListener('install', event => {
+    console.log('[SW] Instalando...');
     event.waitUntil(
-        caches.open('lava-rapido-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js',
-                '/manifest.json'
-            ]);
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('[SW] Cache feito com sucesso');
+            return cache.addAll(URLS_TO_CACHE);
         })
     );
-    self.skipWaiting();
 });
 
+// Ativa e limpa caches antigos
+self.addEventListener('activate', event => {
+    console.log('[SW] Ativando...');
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Intercepta fetch e tenta servir do cache primeiro
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
