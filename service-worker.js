97fcca4766b94d75c5479feb381ecadf1c5a9889
Vinirsf <viniryansf@gmail.com>
2025-04-26 20:58:08 -0300

    🔧 Limpeza, reorganização e atualização completa para Autopass

--- service-worker.js
+++ service-worker.js
@@ -9,35 +9,10 @@ const URLS_TO_CACHE = [
     '/icons/icon-512.png'
 ];
 
-// Instala o SW e faz cache dos arquivos
-self.addEventListener('install', event => {
-    console.log('[SW] Instalando...');
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('[SW] Cache feito com sucesso');
-            return cache.addAll(URLS_TO_CACHE);
-        })
-    );
+self.addEventListener('install', (e) => {
+    console.log('Service Worker instalado');
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener('activate', event => {
-    console.log('[SW] Ativando...');
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
-    );
-});
-
-// Intercepta fetch e tenta servir do cache primeiro
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+    console.log('Service Worker interceptou:', e.request.url);
 });
